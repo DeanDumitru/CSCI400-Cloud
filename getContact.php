c840619edfe32554433069c16d0d1ad72d6a5b66
DeanDumitru <dean_dumitru@yahoo.com>
2017-01-30 03:37:05 +0200

    Maybe fixed the submit button

--- getContact.php
+++ getContact.php
@@ -1,11 +1,17 @@
 <?php
-    $title = $_POST["author"]; //You have to get the form data
-    $gain = $_POST["email"];
-    $offset = $_POST["subject"];
-    $file = fopen('contactLogs.txt', 'w+'); //Open your .txt file
-    ftruncate($file, 0); //Clear the file to 0bit
-    $content = $title. PHP_EOL .$gain. PHP_EOL .$offset;
-    fwrite($file , $content); //Now lets write it in there
-    fclose($file ); //Finally close our .txt
-    die(header("Location: ".$_SERVER["HTTP_REFERER"]));
+   if isset($_POST['submit']){
+    $Fname = $_POST["author"];
+    $email = $_POST["email"];
+    $leader = $_POST["subject"];
+    $industry = $_POST["text"];
+
+    $openFile = fopen("contactLogs.txt",'a');
+        $data = "\t"."{$Fname}";
+        $data .= "\t"."{$email}";
+        $data .= "\t"."{$leader}";
+        $data .= "\t"."{$industry}";
+
+    fwrite($openFile,$data);
+    fclose($openFile);
+}
 ?>
