7a275a154ed44ec4d158ab68d07bef5e57b8933f
ChrisDrech92 <Drechslerc@uindy.edu>
2017-01-30 21:06:45 -0500

    Fixed again...

--- PrimeNumber.php
+++ PrimeNumber.php
@@ -1,7 +1,5 @@
 <?php
-<<<<<<< HEAD
  $number = $_POST["num"]; 
-<<<<<<< HEAD
 
     if($_POST["num"] == 1) {
         echo $_POST["num"]." is not a prime number";
@@ -34,41 +32,4 @@
     echo $_POST["num"]." is a prime number";
 	die("");
 
-=======
-
-    if($_POST["num"] == 1) {
-        echo $_POST["num"]." is not a prime number";
-    }
-	
-	if($_POST["num"] < 1) {
-        echo $_POST["num"]." is not a prime number";
-=======
- 
-function isPrime($num) {
-    if($num == 1) {
-        echo $num." is not a prime number";
->>>>>>> origin/master
-    }
-
-    if($num == 2) {
-        echo $num." is a prime number";;
-    }
-
-    if($num % 2 == 0) {
-        echo $num." is not a prime number";
-    }
-
-    for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2) {
-
-        if($num % $i == 0)
-
-            echo $num." is not a prime number";
-    }
-<<<<<<< HEAD
-    echo $_POST["num"]." is a prime number";
->>>>>>> origin/master
-=======
-    echo $num." is a prime number";
-}
->>>>>>> origin/master
 ?>
